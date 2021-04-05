7c0bdca7ddcfad28600e3264d75aa123147b9b37
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-04-05 00:55:24 -0300

    Exercicio 8

---
+++ ex08.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+if (soma > 10):
+    print("Número maior que 10")
+else:
+    print("Número menor ou igual á 10")
