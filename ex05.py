0e0b7c6a6f0ea6103ac50f0f04345b9c0e034902
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-04-05 00:56:11 -0300

    Exercicio 5

--- ex05.py
+++ ex05.py
@@ -1,7 +1,6 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 num3 = int(input("Digite o terceiro número: "))
-
 if (num1 < num2 and num1 < num3):
     if (num3 > num2):
         print("Os números foram digitados em ordem crescente.")
